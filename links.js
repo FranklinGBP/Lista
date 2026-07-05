0a85292a85a63295a54628917c88bc23dbc9dcb0
FranklinGBP <33864496+FranklinGBP@users.noreply.github.com>
2026-07-05 18:15:59 +0200

    Añadir archivo de enlaces de compra

---
+++ links.js
@@ -0,0 +1,22 @@
+// Añade aquí los enlaces de compra.
+// Mantén el id del producto a la izquierda y pega el enlace entre comillas.
+// Ejemplo:
+// "panales-talla-1": "https://www.amazon.es/...",
+
+window.PRODUCT_LINKS = {
+  "panales-talla-1": "",
+  "toallitas-agua": "",
+  "crema-panial": "",
+  "bodys-algodon": "",
+  "pijamas": "",
+  "muselinas": "",
+  "mantita": "",
+  "termometro": "",
+  "banera": "",
+  "gel-bebe": "",
+  "cambiador": "",
+  "chupetes": "",
+  "biberones": "",
+  "baberos": "",
+  "mochila-panales": ""
+};
